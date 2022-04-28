80673e687ba47fe716bc1ee843aac1e28ab3d2d5
Mark Graham <markgraham@Marks-MacBook-Pro.local>
2022-04-28 12:58:38 +0100

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace KeepOnTruckin.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
